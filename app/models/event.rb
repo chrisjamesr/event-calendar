afc0d2e050d07cdb359321173f35d0a1fe48a635
chrisjamesr <chrisjames.r@gmail.com>
2018-06-24 21:14:49 -0400

    add validations for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+
+  validates :name, uniqueness: {case_sensitive: false}
+  validates :location, presence: {allow_blank: false}
+  # validates :time, :date, presence: {allow_blank: false}
 end
