5ffa9037d1729411e616132b541aad713821d4d2
chrisjamesr <chrisjames.r@gmail.com>
2018-06-24 21:22:30 -0400

    build additional actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,30 @@ class EventsController < ApplicationController
     render json: event, status: 201
   end
 
+  def index
+    events = Event.all 
+    render json: events, status: 200
+  end
+
+  def show
+    event = Event.find(params[:id])
+    render json :event, status: 200
+  end
+
+  def update
+    event = Event.find(params[:id])
+    event.update(event_params)
+    render json :event, status: 200
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+    render json: {
+      status: 200,
+      message: "Event has been destroyed."
+    }
+  end
 
 
   private
