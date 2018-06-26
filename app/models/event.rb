ca8ba7fbbd0912aaa7ca5cdfde447736e573aa7f
chrisjamesr <chrisjames.r@gmail.com>
2018-06-26 19:21:34 -0400

    create date and time instance variables

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,17 @@
 class Event < ApplicationRecord
 
   validates :name, uniqueness: {case_sensitive: false}
-  validates :location, presence: {allow_blank: false}
-  # validates :time, :date, presence: {allow_blank: false}
+  validates :location, presence: {allow_blank: true}
+  validates :date_time, presence: {allow_blank: false}
+
+  attr_reader :date, :time
+
+  def date
+    @date = self.date_time.strftime('%m-%d-%Y')
+
+  end
+
+  def time
+    @time = self.date_time.strftime('%I:%M %p')
+  end
 end
