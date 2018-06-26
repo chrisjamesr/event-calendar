7763b32a8b8178f5062d4001356d509a317b661c
chrisjamesr <chrisjames.r@gmail.com>
2018-06-26 18:38:29 -0400

    run migration to rename description column to notes:

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_26_222253) do
+ActiveRecord::Schema.define(version: 2018_06_26_223610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string "name"
-    t.string "description"
+    t.string "notes"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
