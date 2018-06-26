4c0176e75c63e7984a2b9db895cc6316ce7f0757
chrisjamesr <chrisjames.r@gmail.com>
2018-06-26 18:25:35 -0400

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_24_190535) do
+ActiveRecord::Schema.define(version: 2018_06_26_222253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
-    t.date "date"
-    t.time "time"
     t.string "name"
     t.string "description"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "date_time"
   end
 
 end
