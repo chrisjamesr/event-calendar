033a087509302fdc01a0ec21afbae2085ff62626
chrisjamesr <chrisjames.r@gmail.com>
2018-06-27 13:28:14 -0400

    add classNames and additional mark-up to display Event information

--- client/src/components/EventTile.js
+++ client/src/components/EventTile.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import '../styles/eventTile.css'
 
 const EventTile=({event})=>{
   return (
-    <div>
-      <h3>{event.name}</h3>
-      <p>{event.date}, {event.time}</p>
-      <p>{event.location}</p>
-      <p>{event.notes}</p>
+    <div className="EventTile">
+      <aside className="Event-sidebar">
+        <div className="Event-date-header">{event.date}</div>
+        <div className="Event-time">{event.time}</div> 
+      </aside>  
+      <main>
+        <div className="Event-name-header">
+          <h3>{event.name}</h3>
+        </div>  
+        <div className="Event-details">
+          <p>{event.location}</p>
+          <p>{event.notes}</p>
+        </div> 
+      </main>  
     </div>
   )
 }
