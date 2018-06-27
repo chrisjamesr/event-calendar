aa2d2ccdc45ec21f304ec7c73a26135a73703308
chrisjamesr <chrisjames.r@gmail.com>
2018-06-26 22:45:49 -0400

    add thunk middleware to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,12 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/index'
+import thunk from 'redux-thunk'
 
 export function configureStore(){
   return createStore(
-    rootReducer, 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+     applyMiddleware(thunk)
   );
 }
 
