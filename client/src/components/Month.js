c4b6f439d5a935fad561c8bd79a77ffd2f963db0
chrisjamesr <chrisjames.r@gmail.com>
2018-07-01 02:02:39 -0400

    add month name and year

--- client/src/components/Month.js
+++ client/src/components/Month.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Month(props){
+const Month = props => {
   return(
     <div>
-    <h1>{props.month}</h1>
+    <h1>{props.name}, {props.year}</h1>
     </div>
   )
 }
+export default Month
