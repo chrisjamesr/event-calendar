71009ed14e443b2d24c15db4400af985214dc420
chrisjamesr <chrisjames.r@gmail.com>
2018-07-01 16:07:34 -0400

    import EventsList component

--- client/src/containers/EventsContainer.js
+++ client/src/containers/EventsContainer.js
@@ -2,29 +2,17 @@ import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { fetchEvents } from '../actions/fetchEvents'
-import EventTile from "../components/EventTile"
+import EventsList from '../components/EventsList'
 
 
 export class EventsContainer extends React.Component {
-  
 
   componentDidMount(){
     this.props.fetchEvents()
   }
 
   render(){
-    const events = this.props.events.map((e,i)=>{
-      return <EventTile event={e} key={i} id={e.id}/>
-    })
-
-    return(
-    <div>
-      <h1>Events</h1>
-      {events}
-      <p></p>
-    </div>
-
-    );
+    return <EventsList events={this.props.events} />    
   }
 }
 
