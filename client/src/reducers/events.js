8cf143274787d024a0a627ef9cea2ed39cb062a7
chrisjamesr <chrisjames.r@gmail.com>
2018-07-01 22:42:54 -0400

    add filter to prevent duplicate events

--- client/src/reducers/events.js
+++ client/src/reducers/events.js
@@ -1,7 +1,7 @@
 export default function eventsReducer(state =[], action){
   switch (action.type) {
     case 'FETCH_EVENTS':
-      let events = state.concat(action.payload)
+      let events = state.concat(action.payload.filter(event=> !state.includes(event)))
       return events
     default:
       return state
