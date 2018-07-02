2ff7d1603a2543a62d34e4b9fe7c34b7be0d5044
chrisjamesr <chrisjames.r@gmail.com>
2018-07-01 22:31:44 -0400

    import Navbar, add Navlinks for schedule, calendar, and add event

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ import '../styles/Navbar.css';
 const Navbar =() => {
   return (
     <div className="nav-header">
-      <NavLink to="/" exact className="link">Schedule View</NavLink>
+      <NavLink to="/schedule" exact className="link">Schedule View</NavLink>
       <NavLink to="/calendar" className="link">Calendar View</NavLink>
       <NavLink to="/add-event" className="link">Add event</NavLink>
     </div>
