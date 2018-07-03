2e17bfcef527878afa38bb2003da71a7c3de9ca4
chrisjamesr <chrisjames.r@gmail.com>
2018-07-03 13:01:49 -0400

    add action to post event data to API. Response includes event as stored in db

--- client/src/actions/createEvent.js
+++ client/src/actions/createEvent.js
@@ -1,13 +1,17 @@
 import fetch from 'isomorphic-fetch'
 
-export function fetchEvents(){
+export function createEvent(event){
   return function(dispatch){
-    dispatch({type: 'LOADING_EVENTS'});
-    return fetch('http://localhost:3000/api/events')
+    dispatch({type: 'CREATE_EVENT'});
+    return fetch('http://localhost:3000/api/events',{
+      body: JSON.stringify(event),
+      headers: { 'content-type': 'application/json' },
+      method: 'POST'
+    })
       .then(response => response.json())
       .then(events => dispatch({
-        type: 'FETCH_EVENTS',
-        payload: events
+        type: 'CREATE,EVENT',
+        payload: event
       })
     ) 
   }
@@ -15,4 +19,3 @@ export function fetchEvents(){
 
 export function monthEvents(){
   
-}
