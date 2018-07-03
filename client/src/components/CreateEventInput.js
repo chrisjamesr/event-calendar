5b5a7d7d738cccc6c740e95148d463938c649eec
chrisjamesr <chrisjames.r@gmail.com>
2018-07-03 12:59:52 -0400

    relocate and rename functional component for creating new events

--- client/src/components/CreateEventInput.js
+++ client/src/components/CreateEventInput.js
@@ -8,16 +8,36 @@ import { createEvent } from '../actions/fetchEvents'
 export class EventInput extends React.Component {
   constructor(){
     this.state = {
-
+      event: {
+        name: '',
+        datetime: '',
+        location: '',
+        details: ''
+      },
     }
   }
 
+  
   componentDidMount(){
-    this.props.createEvent()
+    this.props.createEvent(event)
   }
 
   render(){
-    
+    return(
+      <div>
+        <form>
+          <h3>New event</h3>
+          <label>Name: </label>
+          <input type="text" onChange={} />
+          <label>Date & Time: </label>
+          <input type="datetime-local" onChange={} />
+          <label>Location: </label>
+          <input type="text" onChange={} />
+          <label>Details: </label>
+          <textarea onChange={} />
+        </form>
+      </div>
+    )
   }
 }
 
