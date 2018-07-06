fb556dee7cd10e5240f250b96c39b10213bb08ee
chrisjamesr <chrisjames.r@gmail.com>
2018-07-05 22:15:09 -0400

    import redux functions, not used

--- client/src/containers/MonthsContainer.js
+++ client/src/containers/MonthsContainer.js
@@ -1,21 +1,33 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { fetchEvents } from '../actions/fetchEvents'
 import Month from '../components/Month'
 import {calendar} from '../utils/calendar'
 
 
-export default class MonthsContainer extends React.Component {
+class MonthsContainer extends React.Component {
+
+  componentDidMount(){
+    
+  }
+
   render(){
-    const months = calendar
-    debugger 
+    const months = calendar;
+    
     return(
       <div>
         {
           months.map((m,i)=> {
-            return <Month id={`${m.monthName}-${m.year}`} key={i} name={m.monthName} year={m.year} /> 
+            return <Month id={`${m.monthName}-${m.year}`} key={i} name={m.monthName} year={m.year} month={m.month} /> 
           })
         }
       </div>
     )
   }
-
 }
+
+
+
+// export default connect(mapStateToPrsdops, mapDispatchToProps)(MonthsContainer)
+export default MonthsContainer
