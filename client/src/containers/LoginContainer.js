0f736beb431c4175e79b322200e05ceb6ccd19d5
chrisjamesr <chrisjames.r@gmail.com>
2018-07-07 18:34:56 -0400

    create container component for login component to handle api calls for authorization

---
+++ client/src/containers/LoginContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginInput from '../components/LoginInput'
+
+export class LoginContainer extends React.Component{
+  state
+
+  render(){
+    return(
+      <div>
+        <LoginInput />
+      </div>
+    )
+  }
+}
