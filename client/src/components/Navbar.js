8e8ef61668d4d05dccf70fb78e8a9d5204a52a7e
chrisjamesr <chrisjames.r@gmail.com>
2018-07-07 18:36:28 -0400

    add login component to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {NavLink } from 'react-router-dom';
 import '../styles/Navbar.css';
+import {LoginContainer} from '../containers/LoginContainer'
   
 const Navbar =() => {
   return (
+
     <div className="nav-header">
       <NavLink to="/schedule" exact className="link">Schedule View</NavLink>
       <NavLink to="/calendar" className="link">Calendar View</NavLink>
       <NavLink to="/add-event" className="link">Add event</NavLink>
+      <LoginContainer />
     </div>
   )
 }
