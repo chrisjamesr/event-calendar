b5304e7276c5ee0a5ff94e8ff2b82d355178595e
chrisjamesr <chrisjames.r@gmail.com>
2018-07-08 11:44:23 -0400

    modify add create action and user params method

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,16 @@
+class UserController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
