71937473891bd5825ea39c7c004b5dc606d02509
chrisjamesr <chrisjames.r@gmail.com>
2018-07-08 14:15:26 -0400

    move users#create and user_token#create routes into API scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#home'
-  post 'user_token' => 'user_token#create'
+  # post 'user_token' => 'user_token#create'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
+    post 'user_token' => 'user_token#create'
     resources :events, :except => [:new, :edit]
+    resources :users, :only => [:create, :index]
   end
 end
