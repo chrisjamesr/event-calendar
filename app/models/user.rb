8a587e6431875fa1b289e83d0b57374ceb1a7f42
chrisjamesr <chrisjames.r@gmail.com>
2018-07-07 22:53:16 -0400

    add additional validations, add method to return token payload for Knock implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, :name, :password, allow_blank: false, 
+  validates :email, :name, :password, presence: true, allow_blank: false
   validates :email, :name, uniqueness: {case_sensitive: false}
   validates_format_of :email, with: /@/
+
+  def to_token_payload
+        {
+            sub: id,
+            email: email
+        }
+    end
   
 end
