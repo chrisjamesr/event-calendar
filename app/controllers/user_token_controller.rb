76b41a01ea44c2944fda0fa82a552c31371b1daa
chrisjamesr <chrisjames.r@gmail.com>
2018-07-09 21:48:05 -0400

    add action to create jwt for existing user and strong param method

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,4 +1,18 @@
-class UserTokenController < Knock::AuthTokenController
-  skip_before_action :verify_authenticity_token, raise: false
+class UserTokenController < ApplicationController
+  # skip_before_action :authenticate
+  
+  def create
+    @user = User.find_by(email: auth_params[:email])
+    if @user.authenticate(auth_params[:password])
+      jwt = Auth.issue({user: @user.id})
+      render json: {jwt: jwt}
+    end
+  end
+
+  private
+
+  def auth_params
+    params.require(:auth).permit(:email, :password)
+  end  
 
 end
