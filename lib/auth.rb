7551bc3ec714a85c01ceec8b4c97dc39b03fc665
chrisjamesr <chrisjames.r@gmail.com>
2018-07-09 21:43:27 -0400

    add expiration time to Auth, for future

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,13 +1,28 @@
 require 'jwt'
 
 class Auth
+  
+    ALGORITHM = "HS256"
+    exp = Time.now.to_i + 4 * 3600
 
-  def self.issue(payload)
+    def self.issue(payload)
+      JWT.encode(
+        payload,
+        auth_secret,
+        ALGORITHM)
+    end
 
-  end
+    def self.decode(token)
+      JWT.decode(token,
+        auth_secret,
+        true,
+        {algorithm: ALGORITHM}
+      ).first
+    end
 
-  def self.decode(token)
-
-  end
-
+  private
+    def self.auth_secret
+      Rails.application.credentials.auth_secret
+    end
+  
 end
