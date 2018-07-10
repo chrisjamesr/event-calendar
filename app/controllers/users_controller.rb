09ac4b77225549a8b502892749846172ff54ebb4
chrisjamesr <chrisjames.r@gmail.com>
2018-07-09 21:48:44 -0400

    modify create action to return jwt upon successful creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
-
+  skip_before_action :authenticate
+  
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user, status: 201
+    @user = User.new(user_params)
+    if @user.save
+      jwt = Auth.issue({user: @user.id})
+      render json: {jwt: jwt}
     else
-      render json: user.errors
+      render json: @user.errors
     end
   end
 
