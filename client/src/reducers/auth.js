efa90ca8d75e33f2405903cba8f792bae26b0b43
chrisjamesr <chrisjames.r@gmail.com>
2018-07-11 20:25:00 -0400

    add auth reducer for login with jwt token

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,8 +1,13 @@
-export default function authReducer(state={jwt: ''}){
+export default function authReducer(state={jwt: ''}, action){
   switch (action.type){
-    case 'LOGIN':
-      const jwt = action.payload['jwt']
-      return jwt
+    case 'LOGIN_REQUEST':
+      return state
+    case 'LOGIN_SUCCESS':
+    debugger
+      let token = {jwt: action.payload}      
+      return token
+    case 'LOGIN_FAILURE':
+      return Object.assign({},state, {msg: action.payload})
     default:
       return state
   }
