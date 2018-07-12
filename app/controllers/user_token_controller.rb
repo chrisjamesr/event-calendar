ad2c8099a7b86a72d00e89447893d0a4f55958d4
chrisjamesr <chrisjames.r@gmail.com>
2018-07-11 20:28:38 -0400

    add header status code 403 for if user can't be authenticated

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,11 +1,14 @@
 class UserTokenController < ApplicationController
-  # skip_before_action :authenticate
+  skip_before_action :authenticate
   
   def create
     @user = User.find_by(email: auth_params[:email])
-    if @user.authenticate(auth_params[:password])
+    if @user && @user.authenticate(auth_params[:password])
       jwt = Auth.issue({user: @user.id})
       render json: {jwt: jwt}
+    else 
+      head 403
+      # render json: {error: "Unable to Authenticate"}, status: 403  
     end
   end
 
