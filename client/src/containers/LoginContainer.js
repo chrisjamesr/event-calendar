c1055261f66706cbbe8ac1c9f800811b773034c4
chrisjamesr <chrisjames.r@gmail.com>
2018-07-11 21:37:07 -0400

    add proptypes package

--- client/src/containers/LoginContainer.js
+++ client/src/containers/LoginContainer.js
@@ -1,14 +1,55 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import { bindActionCreators } from 'redux'
 import LoginInput from '../components/LoginInput'
+import {login} from '../actions/authActions'
+
+
 
 export class LoginContainer extends React.Component{
-  state
+  constructor(){
+    super()
+    this.state = {
+      auth: {
+        email: '',
+        password: ''
+      }
+    }
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onChange = (event) => {
+    this.setState({
+      ...this.state,
+      auth: {
+        ...this.state.auth,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.props.login({auth: this.state.auth})
+  }
 
   render(){
     return(
       <div>
-        <LoginInput />
+        <LoginInput 
+          handleChange={this.onChange}
+          handleSubmit={this.onSubmit}
+          email={this.state.auth.email}
+          password={this.state.auth.password}
+          />
       </div>
     )
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({login}, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(LoginContainer)
