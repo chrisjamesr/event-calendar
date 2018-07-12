7da3af8a349e4959565e41918c27d953444b23da
chrisjamesr <chrisjames.r@gmail.com>
2018-07-12 12:56:23 -0400

    rename login function to signin

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,6 @@
 import fetch from 'isomorphic-fetch'
 
-export function login(auth){
+export function signin(auth){
   return function(dispatch){
     dispatch({type: 'LOGIN_REQUEST'})
     return fetch('http://localhost:3000/api/user_token',{
@@ -25,6 +25,32 @@ export function login(auth){
       }) 
   }
 }    
+
+export function signup(auth){
+  return function(dispatch){
+    dispatch({type: 'LOGIN_REQUEST'})
+    return fetch('http://localhost:3000/api/user_token',{
+      body: JSON.stringify(auth),
+      headers: { 'content-type': 'application/json' },
+      method: 'POST'
+      })
+      .then(handleError)
+      .then(res=> res.json())
+      .then(json => {
+        sessionStorage.setItem('jwt', json.jwt)
+        dispatch({
+          type: 'LOGIN_SUCCESS',
+          payload: json.jwt
+        })
+      })  
+      .catch(error=> {
+        dispatch({
+          type: 'LOGIN_FAILURE',
+          payload: error.statusText
+        })
+      }) 
+  }
+}
 
 function handleError(response){
   if (!response.ok) {
