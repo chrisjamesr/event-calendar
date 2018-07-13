9301bdc8a21794dfcb256956f220d3d9b7286d69
chrisjamesr <chrisjames.r@gmail.com>
2018-07-12 21:29:08 -0400

    create input for existing user sign in

---
+++ client/src/components/SignupInput.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SignupInput = ({handleChange, handleSubmit, email, password}) =>{
+  return(
+    <div className="input">
+      <form 
+        className="login-form"
+        onSubmit={handleSubmit}
+      >
+        <div>
+          <input 
+            onChange={handleChange}
+            value = { email }
+            placeholder="email" 
+            type="email" 
+            name="email"
+           /><br />
+          <input 
+            onChange={handleChange}
+            value = { password }
+            placeholder="password" 
+            type="password" 
+            name="password"
+          />
+        </div>
+        <button>Sign In</button>
+      </form>
+    </div>
+  )
+}
+
+// ADD PROPTYPES
+SignupInput.propTypes = {
+  handleChange: PropTypes.func,
+  handleSubmit: PropTypes.func,
+  password: PropTypes.string,
+  email: PropTypes.string
+}
+
+export default LoginInput
