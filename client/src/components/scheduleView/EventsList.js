9a0150085ae839f659fc0960a3d00b719c06e6b9
chrisjamesr <chrisjames.r@gmail.com>
2018-07-12 21:30:22 -0400

    add proptypes to EventsList

--- client/src/components/scheduleView/EventsList.js
+++ client/src/components/scheduleView/EventsList.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Event from './Event'
+import PropTypes from 'prop-types'
+
 const EventsList = ({events}) => {
   const renderEvents = events.map((e,i)=> <Event key={i} id={e.id} event={e}/>)
   return (
@@ -8,6 +10,9 @@ const EventsList = ({events}) => {
         {renderEvents} 
     </div>    
   )
+}
+EventsList.propTypes = {
+  event: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default EventsList
