aa8f8111799251b78a162de319f7a6710d925bf4
chrisjamesr <chrisjames.r@gmail.com>
2018-07-13 13:22:01 -0400

    add class names for user status container div and nav links div

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import {NavLink } from 'react-router-dom';
 import '../styles/Navbar.css';
-import LoginContainer from '../containers/LoginContainer'
+import UserStatusContainer from '../containers/UserStatusContainer'
   
 const Navbar =() => {
   return (
 
     <div className="nav-header">
-      <NavLink to="/schedule" exact className="link">Schedule View</NavLink>
-      <NavLink to="/calendar" className="link">Calendar View</NavLink>
-      <NavLink to="/add-event" className="link">Add event</NavLink>
-      <LoginContainer />
+      <div className="nav-links">
+        <NavLink to="/schedule" exact className="link">Schedule View</NavLink>
+        <NavLink to="/calendar" className="link">Calendar View</NavLink>
+        <NavLink to="/add-event" className="link">Add event</NavLink>
+      </div>
+      <div className="nav-user-status">
+        <UserStatusContainer />
+      </div>
     </div>
+
   )
 }
 
