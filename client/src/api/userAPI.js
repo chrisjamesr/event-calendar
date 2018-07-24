3376f506ddc212b72a661d183bb20de2924086a2
chrisjamesr <chrisjames.r@gmail.com>
2018-07-24 08:42:42 -0400

    modify getToken to utilize modified 'user' object from UserStatusContainer

--- client/src/api/userAPI.js
+++ client/src/api/userAPI.js
@@ -4,9 +4,10 @@ class UserAPI {
   //   !!sessionStorage.jwt ? {'Authorization': `Bearer ${sessionStorage.jwt}`} : null
   //   }
   // }
-  static getToken(auth){
+
+  static getToken(user){
     const tokenRequest = new Request('http://localhost:3000/api/user_token',{
-      body: JSON.stringify({'auth': UserAPI.removeNameProperty(auth)}),
+      body: JSON.stringify({'auth': UserAPI.removeNameProperty(user)}),
       headers: { 'Content-Type': 'application/json' },
       method: 'POST'
       })
@@ -16,8 +17,20 @@ class UserAPI {
     })
   }
 
-  static removeNameProperty({auth}){
-    return {email: auth.email, password: auth.password}
+  static createToken(user){
+    const tokenRequest = new Request('http://localhost:3000/api/users',{
+      body: JSON.stringify({'user': user}),
+      headers: { 'content-type': 'application/json' },
+      method: 'POST'
+      })
+    return fetch(tokenRequest).then(handleError)
+      .then(response=> {
+        return response.json() 
+    })
+  }
+
+  static removeNameProperty(user){
+    return {email: user.email, password: user.password}
   }
 }
 
