f3f061ca6edd85ba00bdfa6885c94a2b6d828bea
chrisjamesr <chrisjames.r@gmail.com>
2018-07-23 21:51:23 -0400

    remove api call from login action. add catch for errors

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,30 +1,29 @@
 import fetch from 'isomorphic-fetch'
+import UserAPI from '../api/userAPI'
 
-export function signin(auth){
+
+export function logIn(auth){
   return function(dispatch){
     dispatch({type: 'LOGIN_REQUEST'})
-    return fetch('http://localhost:3000/api/user_token',{
-      body: JSON.stringify(auth),
-      headers: { 'content-type': 'application/json' },
-      method: 'POST'
+    return UserAPI.getToken(auth) 
+      .catch(error=> {
+        dispatch({
+          type: 'LOGIN_FAILURE',
+          payload: error.statusText
+        })
+        console.error(error)
+        return Promise.reject()
       })
-      .then(handleError)
-      .then(res=> res.json())
       .then(json => {
         sessionStorage.setItem('jwt', json.jwt)
         dispatch({
           type: 'LOGIN_SUCCESS'
         })
       })  
-      .catch(error=> {
-        dispatch({
-          type: 'LOGIN_FAILURE'
-        })
-      }) 
   }
 }    
 
-export function signup(auth){
+export function signUp(auth){
   return function(dispatch){
     dispatch({type: 'LOGIN_REQUEST'})
     return fetch('http://localhost:3000/api/user_token',{
@@ -32,8 +31,6 @@ export function signup(auth){
       headers: { 'content-type': 'application/json' },
       method: 'POST'
       })
-      .then(handleError)
-      .then(res=> res.json())
       .then(json => {
         sessionStorage.setItem('jwt', json.jwt)
         dispatch({
@@ -42,19 +39,14 @@ export function signup(auth){
         })
       })  
       .catch(error=> {
-        dispatch({
-          type: 'LOGIN_FAILURE',
-          payload: error.statusText
-        })
+        // dispatch({
+        //   type: 'LOGIN_FAILURE',
+        //   payload: error.statusText
+        // })
+        console.error(`this didn't work, ${error}`)
       }) 
   }
 }
 
-function handleError(response){
-  if (!response.ok) {
-    throw Error(response.statusText);
-  }
-    return response
-}
 
-function storeToken(){}
+
