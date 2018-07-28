2b12cd3e67a3c5342b68c9ba4730053e3ca8b62e
chrisjamesr <chrisjames.r@gmail.com>
2018-07-28 11:33:29 -0400

    add state model in comments, declare events variable above reducer function

--- client/src/reducers/events.js
+++ client/src/reducers/events.js
@@ -1,17 +1,16 @@
 import initialState from './initialState'
+let events;
 
 export default function eventsReducer(state=initialState.events, action){
   switch (action.type) {
     case 'FETCH_EVENTS':
-      let events = action.payload
-      return events
+      return action.payload
     case 'EVENTS_LOAD_FAILURE':
       return state
     case 'CREATE_EVENT_REQUEST':
       return state
     case 'CREATE_EVENT_SUCCESS':
-      // create new events array with new event added
-      return state
+      return [...state, action.payload]
     case 'CREATE_EVENT_FAILURE':
       return state
     case 'UPDATE_EVENT_REQUEST':
@@ -32,3 +31,12 @@ export default function eventsReducer(state=initialState.events, action){
       return state
   }
 }
+
+// state = [   
+//   { id: event.id,
+//     name: event.name,
+//     description: event.description,
+//     location: event.location,
+//     date_time: event.date_time
+//   }, {event}, {event}
+// ]
