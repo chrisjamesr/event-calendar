56d88e1ffe183d9f0bcf8191ced842b27ce5d965
chrisjamesr <chrisjames.r@gmail.com>
2018-07-27 20:31:16 -0400

    add cases for all unbuilt event actions

--- client/src/reducers/events.js
+++ client/src/reducers/events.js
@@ -5,10 +5,29 @@ export default function eventsReducer(state=initialState.events, action){
     case 'FETCH_EVENTS':
       let events = action.payload
       return events
-    case 'CREATE_EVENT':
-      let event = action.payload
-      debugger
-      return state.concat(event)
+    case 'EVENTS_LOAD_FAILURE':
+      return state
+    case 'CREATE_EVENT_REQUEST':
+      return state
+    case 'CREATE_EVENT_SUCCESS':
+      // create new events array with new event added
+      return state
+    case 'CREATE_EVENT_FAILURE':
+      return state
+    case 'UPDATE_EVENT_REQUEST':
+      return state      
+    case 'UPDATE_EVENT_SUCCESS':
+      // create new events array and replace existing event with modified event
+      return state        
+    case 'UPDATE_EVENT_FAILURE':
+      return state    
+    case 'DESTROY_EVENT_REQUEST':
+      return state
+    case 'DESTROY_EVENT_SUCCESS':
+      // create new events array ommitting destroyed event
+      return state          
+    case 'DESTROY_EVENT_FAILURE':
+      return state
     default:
       return state
   }
