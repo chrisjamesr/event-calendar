6a34e4c0ca99b65a35c54887793f3cb319080da8
chrisjamesr <chrisjames.r@gmail.com>
2018-07-30 21:59:31 -0400

    Creator component for EventShow. Connected to store with state and dispatch mapped to props

---
+++ client/src/containers/EventShowContainer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import moment from 'moment'
+import { readEvent } from '../actions/eventActions'
+import EventShow from '../components/scheduleView/EventShow'
+
+class EventShowContainer extends React.Component {
+  constructor(props){
+    super(props)
+
+  }
+
+  componentDidMount(){
+    debugger
+    this.props.readEvent(this.props.match.params.id)
+  }
+
+  render(){
+    return  <EventShow event={this.props.event} />
+  }
+
+}
+
+const mapStateToProps = (event)=> {
+  return {event}
+}
+
+const mapDispatchToProps = (dispatch)=> {
+  return bindActionCreators({readEvent}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventShowContainer)
