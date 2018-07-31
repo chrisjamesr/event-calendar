8b5587e7df451a1d2c6cedd946f3d7d70005f3fc
chrisjamesr <chrisjames.r@gmail.com>
2018-07-30 22:42:16 -0400

    add currentEventReducer to combine reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
 import events from './events'
 import auth from './auth'
+import currentEvent from './currentEvent'
 
 export default combineReducers({
-  events, auth
+  events, auth, currentEvent
 })
