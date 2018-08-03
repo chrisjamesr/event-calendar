d4dbc289504c5f2b93d58765c963e66e91fbae06
chrisjamesr <chrisjames.r@gmail.com>
2018-08-03 13:17:09 -0400

    add conditional to render AuthContainer in Navbar, unless on at root '/'

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,21 +3,28 @@ import {NavLink } from 'react-router-dom';
 import '../styles/Navbar.css';
 import AuthContainer from '../containers/AuthContainer'
   
-const Navbar =() => {
+const Navbar =({match, history, location}) => {
+
+  const renderAuthContainer = (location) =>{
+    
+    return location.pathname !== "/" ? <AuthContainer history={history}/> : null
+  }
+
   return (
 
     <nav className="nav-header">
       <div className="nav-links">
-        <NavLink to="/" exact className="link">Home</NavLink>
-        <NavLink to="/events" className="link">Schedule View</NavLink>
+        
+        <NavLink to="/events" className="link">Upcoming Events</NavLink>
         { 
          // <NavLink to="/calendar" className="link">Calendar View</NavLink>
-        }  
+        }
+        <NavLink to="/" exact className="link">Home</NavLink>  
         <NavLink to="/events/new" className="link">Add event</NavLink>
       </div>
       <div className="nav-user-status">
         {
-         // <AuthContainer />
+         renderAuthContainer(location)
         }  
       </div>
     </nav>
