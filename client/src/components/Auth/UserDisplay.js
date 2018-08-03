3cdf40ab0b0399a23442f8a032281febb35833f1
chrisjamesr <chrisjames.r@gmail.com>
2018-08-03 13:14:30 -0400

    add link to 'my events' page

--- client/src/components/Auth/UserDisplay.js
+++ client/src/components/Auth/UserDisplay.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import '../../styles/Navbar.css'
 
 const UserDisplay = ({userName, logOut}) => {
   return (
     <div className="user-display">
       <p>Logged in as {userName} </p>
+      <NavLink to="/" exact className="link">My Events</NavLink>  
       <button onClick={logOut}>Log Out</button>
     </div>
   )
