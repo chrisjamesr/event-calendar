ca7ff284bee8ffff322c69f559e38f6d9a963b31
chrisjamesr <chrisjames.r@gmail.com>
2018-08-04 15:59:14 -0400

    fix typo

--- db/migrate/20180804191620_create_user_events.rb
+++ db/migrate/20180804191620_create_user_events.rb
@@ -2,7 +2,7 @@ class CreateUserEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :user_events do |t|
       t.integer :user_id
-      t.ingeter :event_id
+      t.integer :event_id
       t.timestamps
     end
   end
