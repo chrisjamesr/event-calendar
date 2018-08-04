9e848741e00484abeb9cbf4cd1541ebb91ab0af3
chrisjamesr <chrisjames.r@gmail.com>
2018-08-04 16:03:35 -0400

    add associations for user_events

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,5 @@
-class EventSerializer < ActiveModel::Serializer
+class EventSerializer < ActiveModel::Serializer  
   attributes :id, :name, :description, :location, :date_time
+  has_many :user_events
+  has_many :users, through: :user_events
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email
+  has_many :user_events
+  has_many :events, through: :user_events
 end
