bb9b3c6100f17bb521df6174bceae5cd73bee779
chrisjamesr <chrisjames.r@gmail.com>
2018-08-04 15:22:10 -0400

    add associations for user_events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_events
+  has_many :events, :through => :user_events
+  
   has_secure_password
   validates :email, :password, presence: true, allow_blank: false
   validates :email, uniqueness: {case_sensitive: false}
