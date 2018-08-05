681bbff0eb6d80ec415eef47d3363e2176868704
chrisjamesr <chrisjames.r@gmail.com>
2018-08-05 19:51:12 -0400

    add username method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :user_events
   has_many :events, :through => :user_events
-  
+
   has_secure_password
   validates :email, :password, presence: true, allow_blank: false
   validates :email, uniqueness: {case_sensitive: false}
@@ -17,5 +17,9 @@ class User < ApplicationRecord
             email: email
         }
     end
+
+  def username
+    @username = self.email.match(/.+(?=@)/)[0]    
+  end 
   
 end
