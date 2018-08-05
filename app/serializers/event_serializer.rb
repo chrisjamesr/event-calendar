55add7cf623672f913a77161f052b1991885d60f
chrisjamesr <chrisjames.r@gmail.com>
2018-08-05 19:50:40 -0400

    remove user association

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,4 @@
 class EventSerializer < ActiveModel::Serializer  
   attributes :id, :name, :description, :location, :date_time
   has_many :user_events
-  has_many :users, through: :user_events
 end
