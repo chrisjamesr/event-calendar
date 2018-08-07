904ac66216382d1852a7cc8cf8e1431613ab1a6d
chrisjamesr <chrisjames.r@gmail.com>
2018-08-07 13:02:17 -0400

    customize user_events attribute to return only the relevant user info (id & username)

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,12 @@
 class EventSerializer < ActiveModel::Serializer  
-  attributes :id, :name, :description, :location, :date_time
+  attributes :id, :name, :description, :location, :date_time, :user_events
   has_many :user_events
+
+  def user_events
+    object.user_events.collect do |ue|
+      Hash[ue.id => Hash[:id => ue.user_id, :username => ue.user.username]]
+    end  
+  end
+
+
 end
