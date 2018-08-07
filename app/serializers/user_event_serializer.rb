108fbca85331eced49a16052a3651f0bfb000640
chrisjamesr <chrisjames.r@gmail.com>
2018-08-06 22:14:34 -0400

    modify custom attribute methods to return correct info in a hash

--- app/serializers/user_event_serializer.rb
+++ app/serializers/user_event_serializer.rb
@@ -4,11 +4,11 @@ class UserEventSerializer < ActiveModel::Serializer
   belongs_to :event
 
   def user
-    [id: object.user.id, username: object.user.username]
+    {id: object.user.id, username: object.user.username}
   end
 
   def event
-    [id: object.event.id, name: object.event.name]
+    {id: object.event.id, name: object.event.name}
   end
 
 end
