0d9c7f244c878ede764513a565befd9b38d304b1
chrisjamesr <chrisjames.r@gmail.com>
2018-08-06 23:01:09 -0400

    add custom validator

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -2,5 +2,10 @@ class UserEvent < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  validates :user, :event, :presence => true
+  validates :user_id, :event_id, :presence => true
+  validate :unique_rsvp
+
+  def unique_rsvp
+    errors.add(:event_id, "You've already RSVP'd") if UserEvent.where(user_id: self.user_id, event_id: self.event_id).present?
+  end
 end
