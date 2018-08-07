8bca267bfe3ac21daca6877fceb19523cb49a68b
chrisjamesr <chrisjames.r@gmail.com>
2018-08-06 23:00:12 -0400

    add create and destroy methods, test create => OK

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,2 +1,24 @@
 class UserEventsController < ApplicationController
+
+  def create
+    if logged_in?
+      if user_event = UserEvent.create(user_event_params)
+        render json: user_event, status: 200
+      end
+    else
+      render head: 403
+    end
+  end
+
+  def destroy
+    binding.pry
+    logged_in? ? user_event.find(id: user_event_params[:id]).destroy : (head 403)
+  end
+
+  private
+
+  def user_event_params
+    params.require(:rsvp).permit(:id, :user_id, :event_id)
+  end
+
 end
