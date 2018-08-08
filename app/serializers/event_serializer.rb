12e99191263e6a8ec30025ff97a7cec062f99f13
chrisjamesr <chrisjames.r@gmail.com>
2018-08-07 21:11:48 -0400

    return only user info from user_event

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,6 +1,6 @@
 class EventSerializer < ActiveModel::Serializer  
   attributes :id, :name, :description, :location, :date_time, :user_events
-  has_many :user_events
+  # has_many :user_events
 
   def user_events
     object.user_events.collect do |ue|
