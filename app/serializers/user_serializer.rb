1c7bdc6f3ba10500481154fe7b712c852dcef81e
chrisjamesr <chrisjames.r@gmail.com>
2018-08-07 21:13:53 -0400

    return only event info from user_event

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :user_events
-  has_many :user_events
+  # has_many :user_events, serializer: UserEventSerializer
   
-
+  def user_events
+    object.user_events.collect do |ue|
+      Hash[ue.id => Hash[:id => ue.event_id, :name => ue.event.name]]
+    end  
+  end
 
 end
