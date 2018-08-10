9ff5105d1fa05aa31d5510068ea96df0658528b9
chrisjamesr <chrisjames.r@gmail.com>
2018-08-09 21:57:31 -0400

    add initial state, and compose function to wrap apply middleware and redux tools extension

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
-import rootReducer from './reducers/index'
+import { createStore, applyMiddleware, compose } from 'redux'
+import {rootReducer} from './reducers/index'
 import thunk from 'redux-thunk'
+import initialState from './reducers/initialState'
 
 export function configureStore(){
   return createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-     applyMiddleware(thunk)
+    rootReducer, initialState, 
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )    
   );
 }
 
