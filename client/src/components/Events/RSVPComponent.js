3335807e093d8cce7f3bcb15157f3bd3a861c3ae
chrisjamesr <chrisjames.r@gmail.com>
2018-08-12 22:32:02 -0400

    add component to display button and rsvp status

---
+++ client/src/components/Events/RSVPComponent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const RSVPComponent = ({attending, handleClick}) => {
+  return (
+    <div>
+      <input 
+        type="submit" 
+        name="rsvp" 
+        onClick={handleClick} 
+        value={attending ? "Attending" : "RSVP" }
+      />
+    </div>
+  )
+}
+
+RSVPComponent.propTypes = {
+  attending: PropTypes.number
+}
+
+export default RSVPComponent
