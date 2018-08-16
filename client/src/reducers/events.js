27158d968ca45b80f4c494ea655ea8fda6cc36c2
chrisjamesr <chrisjames.r@gmail.com>
2018-08-16 18:39:35 -0400

    add FETCH EVENTS, request and failure cases to reducer

--- client/src/reducers/events.js
+++ client/src/reducers/events.js
@@ -3,8 +3,12 @@ let events;
 
 export default function eventsReducer(state=initialState.events, action){
   switch (action.type) {
-    case 'FETCH_EVENTS': 
+    case 'FETCH_EVENTS_REQUEST': 
+      return state
+    case 'FETCH_EVENTS_SUCCESS': 
       return action.payload
+    case 'FETCH_EVENTS_FAILURE': 
+      return state  
     case 'EVENTS_LOAD_FAILURE':
       return state
     case 'CREATE_EVENT_REQUEST':
