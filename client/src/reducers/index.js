b64f138f704ca0eefe77592428b3cd1b5ff0f902
chrisjamesr <chrisjames.r@gmail.com>
2018-08-16 21:22:27 -0400

    comment out rsvp reducer and import

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,9 @@ import { combineReducers } from 'redux'
 import events from './events'
 import auth from './auth'
 import currentEvent from './currentEvent'
-import rsvp from './rsvp'
+// import rsvp from './rsvp'
 
 export const rootReducer = combineReducers({
-  events, auth, currentEvent, rsvps
+  events, auth, currentEvent
+  // ,rsvp
 })
