ccf2e3d5b7687170fab4838979d207b7373016f4
chrisjamesr <chrisjames.r@gmail.com>
2018-08-20 13:16:56 -0400

    modify attributes. Add method for rsvp, not inuse

--- app/serializers/user_event_serializer.rb
+++ app/serializers/user_event_serializer.rb
@@ -1,9 +1,14 @@
 class UserEventSerializer < ActiveModel::Serializer
-  attributes :id, :user, :event
-  belongs_to :user
-  belongs_to :event
+  attributes :id, :user
+  # attributes :rsvp
+  # belongs_to :user
+  # belongs_to :event
 
-  def user
+  def rsvp
+    Hash[:rsvp_id => object.id, :user_id => object.user_id, :username => object.user.username]
+  end
+
+  def user_id
     {id: object.user.id, username: object.user.username}
   end
 
