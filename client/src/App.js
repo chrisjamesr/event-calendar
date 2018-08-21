b32a01c86867819de7384b20db6197f78e19b373
chrisjamesr <chrisjames.r@gmail.com>
2018-08-20 22:50:32 -0400

    render CreateEventContainer with props on events/new route. Add import HomeContainer

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import EventsContainer from './containers/EventsContainer';
 import MonthsContainer from './containers/MonthsContainer';
 import CreateEventContainer from './containers/CreateEventContainer';
 import EventShowContainer from './containers/EventShowContainer'
-import Home from './components/Home'
+import HomeContainer from './containers/HomeContainer'
 import './styles/App.css';
 
 class App extends React.Component{
@@ -16,11 +16,11 @@ class App extends React.Component{
             <div>
               <Navbar {...this.props} />
                 <div className="container">
-                  <Route exact path="/" component={Home} />
+                  <Route exact path="/" component={EventsContainer} />
                   <Route path="/calendar" component={MonthsContainer} />
                   <Switch>
                     <Route exact path="/events" component={EventsContainer} />
-                    <Route exact path="/events/new" component={CreateEventContainer} />
+                    <Route exact path="/events/new" render={(props)=> <CreateEventContainer {...props} />} />
                     <Route path="/events/:id" component={EventShowContainer} />
                   </Switch>
                 </div>
