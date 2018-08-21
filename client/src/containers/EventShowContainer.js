98a78e5205a213958ec91a9525d4e346863d9a3b
chrisjamesr <chrisjames.r@gmail.com>
2018-08-20 21:41:23 -0400

    add RSVP actions to mapDispatchToProps. Add clickhandler to create/destroy rsvp based on state.attending. Add functions to find existing rsvp on event and check event for changes in rsvp

--- client/src/containers/EventShowContainer.js
+++ client/src/containers/EventShowContainer.js
@@ -3,30 +3,83 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import moment from 'moment'
 import { readEvent } from '../actions/eventsActions'
+import {createRSVP, destroyRSVP} from '../actions/rsvpActions' 
 import EventShow from '../components/Events/EventShow'
 
 class EventShowContainer extends React.Component {
   constructor(props){
     super(props)
+    this.state = {
+      attending: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.toggleAttending = this.toggleAttending.bind(this)
+    this.findRSVP = this.findRSVP.bind(this)
+  }
 
+  handleClick = () => {
+    if (this.props.auth){
+      if (this.findRSVP(this.props.event)) {
+        this.props.destroyRSVP(this.props.event.id, this.findRSVP(this.props.event).id) 
+      } else {
+        this.props.createRSVP(this.props.event.id) 
+      } 
+    }
+  }
+
+  toggleAttending = () => {
+    !!this.findRSVP() ? this.setState({attending: true}) : this.setState({attending: false})
   }
 
   componentDidMount(){
     this.props.readEvent(this.props.history, this.props.match.params.id)
   }
 
+  componentDidUpdate(prevProps, prevState){
+    if (prevProps.event.hasOwnProperty("user_events")) {
+      this.rsvpChange(prevProps, this.props) 
+      ? (
+        this.setState({
+          attending: !!this.findRSVP(this.props.event)
+        })
+      ) : null
+    } else if (this.props.event.hasOwnProperty("user_events")) {
+        this.setState({
+          attending: !!this.findRSVP(this.props.event)
+        })
+    }  
+  }
+  
+  rsvpChange(prevProps, currentProps){
+    return this.findRSVP(prevProps.event) !== this.findRSVP(currentProps.event)
+  }
+  
+  checkEmpty = (obj) => {
+    return Object.keys(obj).length === 0
+  }
+
+  findRSVP = (e) => {
+    return e.user_events.find(ue => ue.user_id === parseInt(sessionStorage.user_id)) || null
+  }  
+
   render(){
-    return  <EventShow event={this.props.event} />
+    return (
+      <EventShow 
+        event={this.props.event} 
+        attending = {this.state.attending}
+        handleClick={this.handleClick}
+      />
+    )  
   }
 
 }
 
-const mapStateToProps = ({currentEvent})=> {
-  return {event: currentEvent}
+const mapStateToProps = ({ currentEvent, auth})=> {
+  return {event: currentEvent, auth}
 }
 
 const mapDispatchToProps = (dispatch)=> {
-  return bindActionCreators({readEvent}, dispatch)
+  return bindActionCreators({readEvent, createRSVP, destroyRSVP}, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventShowContainer)
