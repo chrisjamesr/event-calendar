5d281d93ee4cda801637f3bbdf7568514d863a6b
chrisjamesr <chrisjames.r@gmail.com>
2018-08-21 17:58:34 -0400

    modify serializer to include :attending in user_events

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :user_events
-  # has_many :user_events, serializer: UserEventSerializer
+  has_many :user_events
   
   def user_events
     object.user_events.collect do |ue| 
-      Hash[ue.id => Hash[:id => ue.event_id, :username => ue.event.name]]
+      Hash[:id => ue.id , :user_id => ue.event_id, :username => ue.event.name, :attending => ue.attending]
     end
   end
 
