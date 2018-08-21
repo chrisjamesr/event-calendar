2e2c97cad3e76e270fb0f54a5598e762857e92c2
chrisjamesr <chrisjames.r@gmail.com>
2018-08-20 22:17:19 -0400

    remove authContainer and add auth component from comment out nav links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,30 +5,26 @@ import AuthContainer from '../containers/AuthContainer'
   
 const Navbar =({match, history, location}) => {
 
-  const renderAuthContainer = (location) =>{
-    
-    return location.pathname !== "/" ? <AuthContainer history={history}/> : null
-  }
-
   return (
 
     <nav className="nav-header">
       <div className="nav-links">
         
-        <NavLink to="/events" className="link">Upcoming Events</NavLink>
+        {
+          // <NavLink to="/events" className="link">Upcoming Events</NavLink>
+        }
         { 
          // <NavLink to="/calendar" className="link">Calendar View</NavLink>
         }
-        <NavLink to="/" exact className="link">Home</NavLink>  
+        {
+          // <NavLink to="/" exact className="link">Home</NavLink>  
+        }  
         <NavLink to="/events/new" className="link">Add event</NavLink>
       </div>
       <div className="nav-user-status">
-        {
-         renderAuthContainer(location)
-        }  
+        <AuthContainer history={history} />
       </div>
     </nav>
-
   )
 }
 
