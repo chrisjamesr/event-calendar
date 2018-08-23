2c1b849691f481e35dfbe6d8b974cbcd0e8f9896
chrisjamesr <chrisjames.r@gmail.com>
2018-08-23 12:52:45 -0400

    Wrap username in nav link for future link to 'My Events'. Add link to All Events

--- client/src/components/Auth/UserDisplay.js
+++ client/src/components/Auth/UserDisplay.js
@@ -5,8 +5,12 @@ import '../../styles/Navbar.css'
 const UserDisplay = ({userName, logOut}) => {
   return (
     <div className="user-display">
-      <p>Logged in as {userName} </p>
-      <NavLink to="/" exact className="link">My Events</NavLink>  
+      <p>Logged in as
+        <br/>  
+        <NavLink to="/" exact className="link">{userName} </NavLink>  
+        <br/>
+        <NavLink to="/" exact className="link"> All Events</NavLink>  
+      </p>
       <button onClick={logOut}>Log Out</button>
     </div>
   )
