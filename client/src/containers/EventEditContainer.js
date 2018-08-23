e5ec577909b9c21dd967831d05d40ea20d329818
chrisjamesr <chrisjames.r@gmail.com>
2018-08-23 13:05:48 -0400

    create container to modify existing event

---
+++ client/src/containers/EventEditContainer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import {connect} from 'react-redux'
+// Modify CreateEventInput to serve as common component
+// Connect component to store
+// Add Patch & Delete actions & API methods
+// Add conditional for event creator
+// add reducer cases to currentEvent.js/events.js
+
+class EventEditContainer extends React.Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return(
+      <div>
+    </div>
+    )
+  }
+}
+
+export default EventEditContainer
