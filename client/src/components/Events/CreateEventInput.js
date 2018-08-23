d6e4a1a4a8b3dfffd354578a02c80c554facd333
chrisjamesr <chrisjames.r@gmail.com>
2018-08-23 13:12:06 -0400

    Add destructured event object. Add value attribute to inputs

--- client/src/components/Events/CreateEventInput.js
+++ client/src/components/Events/CreateEventInput.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../../styles/form.css'
 
 
-const CreateEventInput = ({handleSubmit, handleChange}) => {
+const CreateEventInput = ({handleSubmit, handleChange, event}) => {
   
   return(
     <div className="input">
@@ -10,19 +10,19 @@ const CreateEventInput = ({handleSubmit, handleChange}) => {
       <form className="input-form" onSubmit={handleSubmit}>
         <div>
           <label>Name: </label>
-          <input type="text" name="name" onChange={handleChange} />
+          <input type="text" name="name" onChange={handleChange} value={event.name}/>
         </div>
         <div>
           <label>Date & Time: </label>
-          <input type="datetime-local" name="date_time" onChange={handleChange} />
+          <input type="datetime-local" name="date_time" onChange={handleChange} value={event.date_time}/>
         </div>
         <div>
           <label>Location: </label>
-          <input type="text" name="location" onChange={handleChange} />
+          <input type="text" name="location" onChange={handleChange} value={event.location}/>
         </div>
         <div>  
           <label>Description: </label>
-          <textarea name="description" onChange={handleChange} />
+          <textarea name="description" onChange={handleChange} value={event.description}/>
         </div>
         <button>Create Event</button>
       </form>
