cd64a7f47433b661c851dfd884efd96085c02479
chrisjamesr <chrisjames.r@gmail.com>
2018-08-22 22:54:47 -0400

    add username custom method and attributes other necessary attributes. comment out other custom methods

--- app/serializers/user_event_serializer.rb
+++ app/serializers/user_event_serializer.rb
@@ -1,19 +1,18 @@
 class UserEventSerializer < ActiveModel::Serializer
-  attributes :user_event
-  # attributes :rsvp
+  attributes :id, :user_id, :username, :event_id, :attending
   # belongs_to :user
   # belongs_to :event
 
-  def user_event
-    Hash[:id => object.id, :user_id => object.user_id, :username => object.user.username, :attending=> object.attending]
+  # def user_event
+  #   Hash[:id => object.id, :user_id => object.user_id, :username => object.user.username, :attending=> object.attending]
+  # end
+
+  def username
+    object.user.username
   end
 
-  def user_id
-    {id: object.user.id, username: object.user.username}
-  end
-
-  def event
-    {id: object.event.id, name: object.event.name}
-  end
+  # def event
+  #   {id: object.event.id, name: object.event.name}
+  # end
 
 end
