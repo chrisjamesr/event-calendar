f88dc15f950fbe82b3544109f25589c17a35f7fa
chrisjamesr <chrisjames.r@gmail.com>
2018-09-08 19:59:30 -0400

    change imported stylesheet and change class names

--- client/src/components/Events/Event.js
+++ client/src/components/Events/Event.js
@@ -1,23 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, Route } from 'react-router-dom';
-import '../../styles/event.css'
+import '../../styles/event-index.css'
 import {dateTime} from '../../utils/calendar'
 
 
 const Event=({event, match})=>{
-  const display= {
-      display: "flex",
-      justifyContent: "flexStart",
-      alignItems: "space-between"
-    }
+  
   return (
-    <div style={display}>                    
+    <div className="indexed-event">                    
       <div>
-        <span style={{margin: "0 15px 0 10px"}} className="Event-date-header">{dateTime(event.date_time).indexDate}</span>
+        <span title={dateTime(event.date_time).displayDay} className="event-date">{dateTime(event.date_time).indexDate}</span>
       </div>
-      <div>
-        <Link to={`/events/${event.id}`} style={{margin: "0 0 0 10px"}} >
+      <div >
+        <Link to={`/events/${event.id}`} className="event-name">
           {event.name} 
         </Link>  
       </div>
