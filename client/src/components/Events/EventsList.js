e6aaa2d032c4d984fa93d5dd844472c75e3d9cd1
chrisjamesr <chrisjames.r@gmail.com>
2018-09-08 20:14:40 -0400

    remove inline styling from Event component

--- client/src/components/Events/EventsList.js
+++ client/src/components/Events/EventsList.js
@@ -3,7 +3,7 @@ import Event from './Event'
 import PropTypes from 'prop-types'
 
 const EventsList = ({events, renderEventShow}) => {
-  const renderEvents = events.map((e,i)=> <li style={{listStyle: "none"}} key={i}><Event key={i} id={e.id} event={e}/></li>)
+  const renderEvents = events.map((e,i)=> <li key={i}><Event key={i} id={e.id} event={e}/></li>)
   return (
     <div>
       <div>
@@ -19,5 +19,7 @@ const EventsList = ({events, renderEventShow}) => {
 EventsList.propTypes = {
   event: PropTypes.arrayOf(PropTypes.object)
 }
+
+export default EventsList 
 
 export default EventsList
