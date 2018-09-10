c55e51c6778954407d5105537a806a55a7d9b8b8
chrisjamesr <chrisjames.r@gmail.com>
2018-09-09 23:26:53 -0400

    add year prop and replace 'Events List' header with year. Add year to proptypes

--- client/src/components/Events/EventsList.js
+++ client/src/components/Events/EventsList.js
@@ -2,22 +2,26 @@ import React from 'react'
 import Event from './Event'
 import PropTypes from 'prop-types'
 
-const EventsList = ({events, renderEventShow}) => {
+const EventsList = ({events, year}) => {
+  
+
   const renderEvents = events.map((e,i)=> <li key={i}><Event key={i} id={e.id} event={e}/></li>)
   return (
     <div>
-      <div>
-      <h1>Events List</h1>
-      
+      <div className="year-header">
+        <h3>{year}</h3>
       </div>
-      <ul>
-        {renderEvents} 
-      </ul>  
+      <div className="events-list-container">
+        <ul>
+          {renderEvents} 
+        </ul>
+      </div>  
     </div>    
   )
 }
 EventsList.propTypes = {
-  event: PropTypes.arrayOf(PropTypes.object)
+  event: PropTypes.arrayOf(PropTypes.object),
+  year: PropTypes.string
 }
 
 export default EventsList 
