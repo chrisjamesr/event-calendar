ed325fd71cfd65f7362c6222015770774a51b10c
chrisjamesr <chrisjames.r@gmail.com>
2018-09-16 14:31:39 -0400

    Remove unordered list from return. Remove li from renderEvents array

--- client/src/components/Events/EventsList.js
+++ client/src/components/Events/EventsList.js
@@ -5,20 +5,19 @@ import PropTypes from 'prop-types'
 const EventsList = ({events, year}) => {
   
 
-  const renderEvents = events.map((e,i)=> <li key={i}><Event key={i} id={e.id} event={e}/></li>)
+  const renderEvents = events.map((e,i)=> <Event key={i} id={e.id} event={e}/>)
   return (
     <div>
       <div className="year-header">
         <h3>{year}</h3>
       </div>
-      <div className="events-list-container">
-        <ul>
-          {renderEvents} 
-        </ul>
+      <div className="events-list-year-container">
+        {renderEvents} 
       </div>  
     </div>    
   )
 }
+
 EventsList.propTypes = {
   event: PropTypes.arrayOf(PropTypes.object),
   year: PropTypes.string
