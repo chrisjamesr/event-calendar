5319f83cf48c62ea89ec75da2bcec9a2b0c56a71
chrisjamesr <chrisjames.r@gmail.com>
2018-09-17 20:57:03 -0400

    add route to catch all non matched routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
       resources :events, :only => [:index]
     end
   end
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
