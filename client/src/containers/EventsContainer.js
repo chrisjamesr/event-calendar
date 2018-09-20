a19f59c5e5246a469d05e4e20bd22bf4fbb74d01
chrisjamesr <chrisjames.r@gmail.com>
2018-09-19 21:21:37 -0400

    remove unused import 'Route'

--- client/src/containers/EventsContainer.js
+++ client/src/containers/EventsContainer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
 import { fetchEvents, fetchUserEvents, createEvent, readEvent } from '../actions/eventsActions'
 import EventYears from '../components/Events/EventYears'
 
