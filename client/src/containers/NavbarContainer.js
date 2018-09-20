6112d1b50866b61f9c8e3c26fce0677379311528
chrisjamesr <chrisjames.r@gmail.com>
2018-09-19 21:17:10 -0400

    remove unnecessary constructor

--- client/src/containers/NavbarContainer.js
+++ client/src/containers/NavbarContainer.js
@@ -3,9 +3,6 @@ import {connect} from 'react-redux';
 import NavbarComponent  from '../components/NavbarComponent'
 
 class NavbarContainer extends React.Component{
-  constructor(props) {
-    super(props)
-  }
 
   render(){
     return(
