9322b4156f0c4be62211be06911dd58d0d98177f
chrisjamesr <chrisjames.r@gmail.com>
2018-09-25 20:28:45 -0400

    remove log to console on login success

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -5,21 +5,19 @@ export function logIn(history, user){
   return function(dispatch){
     dispatch({type: 'LOGIN_REQUEST'})
     return UserAPI.getToken(user)
-      .then(
-        ({ status, json }) => {
-          if (status >= 400) {
-            console.log("login failed")
-            console.log(status, json)
-          } else {
-            console.log(status, json)
-            sessionStorage.setItem('jwt', json.jwt)
-            sessionStorage.setItem('user_id', parseInt(json.user_id,10))
-            sessionStorage.setItem('username', user.email.split('@')[0])  
-            dispatch({
-              type: 'LOGIN_SUCCESS'
-            })
-            history.push("/events") 
-          }  
+      .then(({ status, json }) => {
+        if (status >= 400) {
+          console.log("login failed")
+          console.log(status, json)
+        } else {
+          sessionStorage.setItem('jwt', json.jwt)
+          sessionStorage.setItem('user_id', parseInt(json.user_id,10))
+          sessionStorage.setItem('username', user.email.split('@')[0])  
+          dispatch({
+            type: 'LOGIN_SUCCESS'
+          })
+          history.push("/events") 
+        }  
       }    
     )
   }
@@ -29,25 +27,23 @@ export function signUp(history, user){
   return function(dispatch){
     dispatch({type: 'SIGNUP_REQUEST'})
     return UserAPI.createToken(user) 
-      .catch(error=> {
-        dispatch({
-          type: 'SIGNUP_FAILURE',
-          message: error.statusText
-        })
-        console.error(error.statusText)
-        return Promise.reject()
-      })
-      .then(json => {
-        sessionStorage.setItem('jwt', json.jwt)
-        sessionStorage.setItem('user_id', parseInt(json.user_id,10))
-        sessionStorage.setItem('username', user.email.split('@')[0])
-        dispatch({
-          type: 'SIGNUP_SUCCESS'
-        })
-        history.push("/events") 
-      })  
+      .then(({ status, json }) => {
+        if (status >= 400) {
+          console.log("login failed")
+          console.log(status, json)
+        } else {
+          sessionStorage.setItem('jwt', json.jwt)
+          sessionStorage.setItem('user_id', parseInt(json.user_id,10))
+          sessionStorage.setItem('username', user.email.split('@')[0])  
+          dispatch({
+            type: 'SIGNUP_SUCCESS'
+          })
+          history.push("/events") 
+        }  
+      }
+    )
   }
-}
+} 
 
 export function logOut(history){
   return function(dispatch){
