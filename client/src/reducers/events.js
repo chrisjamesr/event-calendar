c5ac708d4bd10734bbafac7de03e9a1d4affc8a6
chrisjamesr <chrisjames.r@gmail.com>
2018-09-27 23:10:39 -0400

    return new events state shape

--- client/src/reducers/events.js
+++ client/src/reducers/events.js
@@ -3,19 +3,19 @@ import initialState from './initialState'
 export default function eventsReducer(state=initialState.events, action){
   switch (action.type) {
     case 'FETCH_EVENTS_REQUEST': 
-      return state
+      return {...state, loading: true}
     case 'FETCH_EVENTS_SUCCESS':
-      return action.payload
+      return {events: action.payload, loading: false}
     case 'FETCH_EVENTS_FAILURE': 
-      return state  
+      return {...state, loading: false}
     case 'EVENTS_LOAD_FAILURE':
       return state
     case 'CREATE_EVENT_REQUEST':
-      return state
+      return {...state, loading: true}
     case 'CREATE_EVENT_SUCCESS':
-      return [...state, action.payload]
+      return {events:[...state, action.payload], loading: false}
     case 'CREATE_EVENT_FAILURE':
-      return state
+      return {...state, loading: true}
     default:
       return state
   }
