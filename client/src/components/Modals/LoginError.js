8dfeccdcf14bdbf37542c57617e0a6994aa89263
chrisjamesr <chrisjames.r@gmail.com>
2018-09-27 22:45:03 -0400

    create modal component for login components

---
+++ client/src/components/Modals/LoginError.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import '../../styles/modal.css'
+
+const LoginModal = ({ handleClose, show, message }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  return (
+
+    <div className={showHideClassName}
+         onClick={handleClose}>
+      <section className="modal-main">
+        {message}
+      </section>
+    </div>
+  );
+};
+
+export default LoginModal
