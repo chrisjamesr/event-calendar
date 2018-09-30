8d40c126c34fb397bb773d8a4f4d241ab01be457
chrisjamesr <chrisjames.r@gmail.com>
2018-09-30 11:45:53 -0400

    return correct state shape from create & modify rsvps actions

--- client/src/reducers/currentEvent.js
+++ client/src/reducers/currentEvent.js
@@ -11,46 +11,40 @@ export default function currentEventReducer(state=initialState.currentEvent, act
   let event;
   switch (action.type) {
   case 'SHOW_EVENT_REQUEST':
-    return state
+    return {...state, loading: true}
   case 'SHOW_EVENT_SUCCESS':
-    return action.payload
+    return {currentEvent: action.payload, loading: false}
   case 'SHOW_EVENT_FAILURE':
     return state        
   case 'UPDATE_EVENT_REQUEST':
-    return state      
+    return {...state, loading: true}
   case 'UPDATE_EVENT_SUCCESS':
     // create new events array and replace existing event with modified event
-    return action.payload
+    return {currentEvent: action.payload, loading: false}
   case 'UPDATE_EVENT_FAILURE':
     return state    
   case 'DELETE_EVENT_REQUEST':
-    return state
+    return {...state, loading: true}
   case 'DELETE_EVENT_SUCCESS':
     // create new events array ommitting destroyed event
-    return emptyEvent   
+    return {currentEvent: emptyEvent, loading: false}
   case 'DELETE_EVENT_FAILURE':
     return state
    case 'CLEAR_EVENT': 
-   return emptyEvent
+   return {currentEvent: emptyEvent, loading: false}
   case 'CREATE_RSVP_REQUEST':
     return state
   case 'CREATE_RSVP_SUCCESS':
-    event = Object.assign({}, state, {user_events: [...state.user_events, action.payload]})
-    return event
+    return {currentEvent: {...state.currentEvent, user_events: [...state.currentEvent.user_events,action.payload]}, loading: false}
   case 'CREATE_RSVP_FAILURE':
     return state  
   case 'UPDATE_RSVP_REQUEST':
-    return state
+    return {...state, loading: true}
   case 'UPDATE_RSVP_SUCCESS':
-    let rsvps = state.user_events.map((ue, i, arr) =>{
+    let rsvps = state.currentEvent.user_events.map((ue, i, arr) =>{
       return ue.id === action.payload.id ? action.payload : ue
-    })      
-    
-    event = Object.assign({}, state, {
-      user_events: rsvps      
-      }
-    )
-    return event
+    })          
+    return {currentEvent: {...state.currentEvent, user_events: rsvps}, loading: false}
   case 'UPDATE_RSVP_FAILURE':
     return state   
   default:
