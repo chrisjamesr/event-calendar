9ea7c1dfa1ebea37a70fe75b0fa797e8ae72fb2d
chrisjamesr <chrisjames.r@gmail.com>
2018-09-30 12:37:47 -0400

    modify reducer to return loading: false for rsvp requests

--- client/src/reducers/currentEvent.js
+++ client/src/reducers/currentEvent.js
@@ -39,7 +39,7 @@ export default function currentEventReducer(state=initialState.currentEvent, act
   case 'CREATE_RSVP_FAILURE':
     return state  
   case 'UPDATE_RSVP_REQUEST':
-    return {...state, loading: true}
+    return {...state, loading: false}
   case 'UPDATE_RSVP_SUCCESS':
     let rsvps = state.currentEvent.user_events.map((ue, i, arr) =>{
       return ue.id === action.payload.id ? action.payload : ue
