40cae0c68cae6b4f36998785fda4502156bb651e
chrisjamesr <chrisjames.r@gmail.com>
2019-03-14 12:18:14 -0400

    add additional config to application tracer settings

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,4 +1,7 @@
 require 'ddtrace'
 Datadog.configure do |c|
-  c.use :rails, service_name: 'event-list-heroku'
+  c.use :rails, service_name: 'event-list-heroku' 
+  c.tracer hostname: 'trace-agent'
+  c.tracer enabled: true
+  c.tracer env: Rails.env
 end
