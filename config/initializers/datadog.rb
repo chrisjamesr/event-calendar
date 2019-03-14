c0d60a30e48fc335791013a868df521ac8bae282
chrisjamesr <chrisjames.r@gmail.com>
2019-03-14 12:25:03 -0400

    remove require ddtrace

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,6 +1,6 @@
-require 'ddtrace'
+
 Datadog.configure do |c|
-  c.use :rails, service_name: 'event-list-heroku' 
+  c.use :rails, service_name: 'event-list-trace-agent' 
   c.tracer hostname: 'trace-agent'
   c.tracer enabled: true
   c.tracer env: Rails.env
